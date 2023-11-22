11e0e5084061fa2492629b208623ff6f4e34eca5
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-11-22 19:04:28 +0200

    more fixes

--- app/(routes)/evenimente/components/EvMarkdown.tsx
+++ app/(routes)/evenimente/components/EvMarkdown.tsx
@@ -5,8 +5,8 @@ import remarkGfm from 'remark-gfm'
 
 const EvMarkdown = ({desc}:{desc: string}) => {
   return (
-    <div className="whitespace-pre-line font-light text-[1em]  text-start sm:text-center lg:text-start md:break-words md:overflow-y-scroll lg:min-h-[400px] p-0 sm:p-2">
-        <Markdown remarkPlugins={[remarkGfm]}>{desc}</Markdown>
+    <div className="whitespace-pre-line font-light text-[1em] text-start sm:text-center lg:text-start md:break-words md:overflow-y-scroll lg:min-h-[400px] p-0 sm:p-2">
+        <Markdown className="overflow-auto h-[400px] break-words" remarkPlugins={[remarkGfm]}>{desc}</Markdown>
     </div>
   )
 }

371248318f100a8b9b61bf2bc12f233508a7f5b0
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-04-16 21:39:52 +0300

    chore - registration done

--- app/(routes)/inscriere-eveniment/page.tsx
+++ app/(routes)/inscriere-eveniment/page.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
 import { PersInfoEvent } from "../components/PersInfoEvent";
+import prismadb from "@/lib/db";
 
 interface pageProps {}
 
-const page: FC<pageProps> = ({}) => {
+const page: FC<pageProps> = async ({}) => {
   return (
     <div className="container mx-auto p-10">
       <h1 className="text-center font-bold text-4xl mb-5">
         Inscrie-te la eveniment
       </h1>
-      <PersInfoEvent/>
+      <PersInfoEvent />
+      
     </div>
   );
 };
--- app/api/inscriereEveniment/route.ts
+++ app/api/inscriereEveniment/route.ts
@@ -1,3 +1,4 @@
+import prismadb from "@/lib/db";
 import { RegistrationDataValidator } from "@/validators";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
@@ -8,12 +9,27 @@ export async function POST(req: NextRequest) {
 
     console.log(payload);
 
-    const data = RegistrationDataValidator.parse(payload)
+    const data1 = RegistrationDataValidator.parse(payload);
 
-    
+    await prismadb.registration.create({
+      data: {
+        nume_copil: data1.nume_copil,
+        prenume_copil: data1.prenume_copil,
+        varsta_copil: data1.varsta_copil,
+        grupa_copil: data1.grupa_copil,
+        nume_parinte: data1.nume_parinte,
+        prenume_parinte: data1.prenume_parinte,
+        nume_parinte_eveniment: data1.nume_parinte_eveniment,
+        email_parinte: data1.email_parinte,
+        sesiune_foto: data1.sesiune_foto === true ? "true" : "false",
+        tombola: data1.tombola === true ? "true" : "false",
+      },
+    });
+
+
 
     return NextResponse.json(
-      { msg: "Imaginea a fost adăugată cu succes !" },
+      { msg: "Te-ai înscris cu succes !" },
       { status: 200 }
     );
   } catch (err) {

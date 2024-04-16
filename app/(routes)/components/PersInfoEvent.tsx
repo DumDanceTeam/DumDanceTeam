1a246912df9fdd362e7a394344fa26511b9e228d
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-04-16 20:05:17 +0300

    chore - desgin pers info event

--- app/(routes)/components/PersInfoEvent.tsx
+++ app/(routes)/components/PersInfoEvent.tsx
@@ -14,6 +14,13 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/Input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const formSchema = z.object({
   nume_copil: z.string().min(2).max(50),
@@ -31,9 +38,6 @@ interface PersInfoEventProps {}
 export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      nume_copil: "",
-    },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -57,9 +61,7 @@ export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
               <FormControl>
                 <Input placeholder="" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
@@ -73,9 +75,7 @@ export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
               <FormControl>
                 <Input placeholder="" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
@@ -89,29 +89,37 @@ export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
               <FormControl>
                 <Input placeholder="" {...field} type="number" />
               </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="grupa_copil"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Grupă vârstă</FormLabel>
+              <FormControl>
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Theme" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="light">4 - 7 ani</SelectItem>
+                    <SelectItem value="dark">8 - 12 ani</SelectItem>
+                    <SelectItem value="system">12 - 16 ani</SelectItem>
+                  </SelectContent>
+                </Select>
+                {/* <Input placeholder="" {...field} /> */}
+              </FormControl>
               <FormDescription>
-                This is your public display name.
+                Grupa de vârstă în care se încadrează copilul tău
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        {/* <FormField
-          control={form.control}
-          name="dropw"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Dropdown</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        /> */}
         <FormField
           control={form.control}
           name="nume_parinte"
@@ -121,9 +129,7 @@ export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
               <FormControl>
                 <Input placeholder="" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
@@ -137,9 +143,7 @@ export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
               <FormControl>
                 <Input placeholder="" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
@@ -153,9 +157,7 @@ export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
               <FormControl>
                 <Input placeholder="" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
@@ -169,14 +171,43 @@ export const PersInfoEvent: FC<PersInfoEventProps> = ({}) => {
               <FormControl>
                 <Input placeholder="" {...field} />
               </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email_parinte"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Sesiune foto Mihai Petre (Opțional)</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} type="checkbox" />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email_parinte"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Doresc să particip și la tombolă (Opțional)</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} type="checkbox" />
+              </FormControl>
               <FormDescription>
-                This is your public display name.
+                Nu-ți fă griji! toți copiii vor fi premiați, dar avem și o
+                tombolă specială cu premii educative pentru copii.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit">Trimite</Button>
       </form>
     </Form>
   );

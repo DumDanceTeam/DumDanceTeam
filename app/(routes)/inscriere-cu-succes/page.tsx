1704eb588d2af161e135cfd7815b6afc0c3cf289
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-04-21 21:08:09 +0300

    chore - text updates

--- app/(routes)/inscriere-cu-succes/page.tsx
+++ app/(routes)/inscriere-cu-succes/page.tsx
@@ -4,20 +4,11 @@ import Image from "next/image";
 const InscriereCuSucces = () => {
   return (
     <div className="container mx-auto py-10">
-      <p className="font-bold text-center text-xl flex items-center justify-center gap-10 mb-2.5">
+      <p className="font-bold text-center text-3xl flex items-center justify-center gap-10 mb-2.5">
         Înscrierea s-a realizat cu succes!{" "}
         <CheckCircle className="w-[100px] h-[100px] text-green-500" />
       </p>
-      <Image
-        width={6000}
-        height={3140}
-        src={"/mihai.png"}
-        quality={100}
-        priority
-        className="w-full h-full rounded-[10px]"
-        alt="event"
-      />
-      <p className="text-center mt-2.5">
+      <p className="text-center mb-2.5">
       🎉Mulțumim pentru înscrierea ta la evenimentul nostru extraordinar,
         &quot;Ritmul DDT: Aradul dansează cu Mihai Petre și Dum Dance
         Team&quot;! Suntem încântați să te avem alături de noi! După încheierea
@@ -28,6 +19,16 @@ const InscriereCuSucces = () => {
         noastre ulterioare și să te pregătești pentru o zi plină de distracție
         și dans! Cu cele mai bune urări, Echipa Dum Dance Team Arad!
       </p>
+      <Image
+        width={6000}
+        height={3140}
+        src={"/mihai.png"}
+        quality={100}
+        priority
+        className="w-full h-full rounded-[10px]"
+        alt="event"
+      />
+     
       {/* <div className="flex lg:flex-row items-center justify-center h-full">
         <div className="flex flex-col lg:flex-row items-center">
           <div>

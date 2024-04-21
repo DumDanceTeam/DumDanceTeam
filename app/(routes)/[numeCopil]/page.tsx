9ff538fa8d47df2a07b4a4f47bacf45233cb744d
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-04-21 12:17:26 +0300

    chore -m nume copil modal

---
+++ app/(routes)/[numeCopil]/page.tsx
@@ -0,0 +1,37 @@
+import { Button, buttonVariants } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+const page = ({ params }: { params: { numeCopil: string } }) => {
+  return (
+    <div className="relative container mx-auto flex justify-center items-center py-20">
+      <div className="border-2 p-10 shadow-2xl rounded-[10px] w-fit">
+        <p className="font-bold text-xl text-center mb-5">
+          <span className="underline underline-offset-2">
+            {params.numeCopil}{" "}
+          </span>
+          te-a invitat la eveniment!
+        </p>
+        <Image
+          src={"/event.jpeg"}
+          width={400}
+          height={400}
+          className="max-w-full max-h-full"
+          alt="event"
+        />
+        <Link
+          href={`/inscriere-eveniment?nume=${params.numeCopil}`}
+          className={cn(
+            buttonVariants({}),
+            "bg-emerald-500 w-full mt-10 hover:bg-[#096c4b]"
+          )}
+        >
+          Acceptă invitația!
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default page;

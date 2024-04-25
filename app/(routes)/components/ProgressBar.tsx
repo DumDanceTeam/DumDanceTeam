0351a258f5cc175f6a71e5dd3a62d3478ce6f13e
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-04-25 21:16:24 +0300

    chore - progress bar updated

--- app/(routes)/components/ProgressBar.tsx
+++ app/(routes)/components/ProgressBar.tsx
@@ -5,66 +5,7 @@ import { FaUserGroup } from "react-icons/fa6";
 import { MdGroupOff } from "react-icons/md";
 
 export function ProgressBar() {
-  const [progress, setProgress] = React.useState(0);
-
-  const currentDate = new Date();
-
-  React.useEffect(() => {
-    if (currentDate.getDate() === 21) {
-      setProgress(1);
-    }
-    if (currentDate.getDate() === 22) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 23) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 24) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 25) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 26) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 27) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 28) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 29) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 30) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 1) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() ===2) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 3) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 4) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 5) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 6) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 7) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-    if (currentDate.getDate() === 8) {
-      setProgress((prevState)=>prevState+5.5);
-    }
-  }, []);
+  const [progress, setProgress] = React.useState(28.5);
 
   return (
     <div className="flex items-center justify-center gap-5">

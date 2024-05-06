04cb85caa43ce3fa67f73e3d95d42041cedb35d1
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-05-06 19:41:46 +0300

    chore - progress bar

--- app/(routes)/components/ProgressBar.tsx
+++ app/(routes)/components/ProgressBar.tsx
@@ -5,7 +5,7 @@ import { FaUserGroup } from "react-icons/fa6";
 import { MdGroupOff } from "react-icons/md";
 
 export function ProgressBar() {
-  const [progress, setProgress] = React.useState(80);
+  const [progress, setProgress] = React.useState(89);
 
   return (
     <div className="flex items-center justify-center gap-5">
